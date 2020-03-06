bf101a792978e0c5697b15aaf6a537e4c4a0215f
Wim Verpoorten <wim.verpoorten@hotmail.com>
2020-03-06 20:14:02 +0100

    overall structure

--- hangman-react/src/App.js
+++ hangman-react/src/App.js
@@ -4,7 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Tijd om een appje te maken!
+        <div className="visuals">
+            <div className="title">Titel/Status Messages</div>
+            <div className="image">Images</div>
+            <div className="text_attempts_left">Text Attempts left</div>
+            <div className="help_button">Help Button</div>
+            <div className="lets_play">Let's Play Animation</div>
+        </div>
+        <div className="word">
+            <div className="word_letter">W</div>
+            <div className="word_letter">O</div>
+            <div className="word_letter">R</div>
+            <div className="word_letter">D</div>
+        </div>
+        <div className="input">
+            <div className="letters">
+                <div className="letter">A</div>
+                <div className="letter">B</div>
+                <div className="letter">C</div>
+                <div className="letter">D</div>
+                <div className="letter">E</div>
+            </div>
+            <div className="new_word">New Word</div>
+        </div>
+        <div className="help">
+            <div className="help_content">help text comes here</div>
+        </div>
     </div>
   );
 }
