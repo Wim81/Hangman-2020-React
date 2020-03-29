2ce6dd7b70d3c41601a3aa0282631e6a70f2922a
Wim Verpoorten <wim.verpoorten@hotmail.com>
2020-03-29 19:10:20 +0200

    prep for build

--- hangman-react/src/App.js
+++ hangman-react/src/App.js
@@ -135,7 +135,7 @@ class HangmanProvider extends Component {
                         wrongLetterCount: newWrongLetterCount,
                         gameOver: newWrongLetterCount < 7 ? false : true,
                         gameStatus: newWrongLetterCount < 7 ? 'in_progress' : 'lost',
-                    }), () => console.log(this.state));
+                    }));
                 },
                 toggleHelpSection: () => {
                     let newHelpShown = !this.state.helpShown;
